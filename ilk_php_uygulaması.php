9b37c88e53e7ecfb593dbb19fa5fdacf76ba4249
Emirhan1q1q <221120221032@ogrenci.ogu.edu.tr>
2024-11-20 23:03:06 +0300

    Değişken nedir çalışması
    $ işaretinin işlevini öğrendim

---
+++ ilk_php_uygulaması.php
@@ -0,0 +1,14 @@
+<?php
+$maasAli=21000;
+$maasMehmet=16000;
+$maasVolkan=24000;
+
+$vergiOrani=0.27;
+$vergiOrani2=0.35;
+
+echo $maasAli-($maasAli*$vergiOrani);
+echo '<br>';
+echo $maasMehmet-($maasMehmet*$vergiOrani);
+echo '<br>';
+echo $maasVolkan -($maasVolkan*$vergiOrani2);
+?>
